67957896cc84bbb61b64a626246b1f63a7859043
Vendula Vankova <vendula.van@gmail.com>
2022-04-08 22:55:02 +0200

    JS file added

--- main.js
+++ main.js
@@ -9,3 +9,18 @@ která vypíše kalorickou hodnotu potraviny.
 BONUS: počítej a zobrazuj celkový počet sněděných kalorií.
 Tj. při klikání na obrázky se postupně sčítají kalorie.
 */
+
+
+
+const obrazky = document.querySelectorAll('.ovoce');
+
+obrazky.forEach((obrazek) => {
+    obrazek.addEventListener('click', zobrazKalorie);
+})
+
+
+function zobrazKalorie(udalost) {
+    let kalorie = udalost.target.dataset.kalorie;
+    console.log('Právě jsi snědla ' + kalorie + ' kalorií.');
+}
+
